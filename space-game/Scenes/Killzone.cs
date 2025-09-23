31edae10e4fcbdeb6e25ed51419cd53311bcafea
costotoad <vkruger@ftstudent.org>
2025-09-23 11:02:20 -0400

    Kill zone updates

--- space-game/Scenes/Killzone.cs
+++ space-game/Scenes/Killzone.cs
@@ -4,6 +4,10 @@ using System;
 public partial class Killzone : Area2D
 { 
 	public void _OnReady() {
-	Print("It works");
+	GD.Print("It works");
+	}
+	
+	public void OnBodyEntered(Node2D body) {
+	GD.Print("It works")
 	}
 }
